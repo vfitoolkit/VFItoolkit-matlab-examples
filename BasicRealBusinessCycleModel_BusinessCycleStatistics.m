8e446eee126d1316ebfb96e584a84ec330644b1a
robertdkirkby <robertdkirkby@gmail.com>
2015-06-28 15:30:38 +1200

    BasicRBC BusCycStats example corrected for change in TimeSeries

--- BasicRealBusinessCycleModel_BusinessCycleStatistics.m
+++ BasicRealBusinessCycleModel_BusinessCycleStatistics.m
@@ -16,8 +16,8 @@ simoptions.simperiods=1000;
 TimeSeriesIndexes=SimTimeSeriesIndexes_Case1(Policy,n_d,n_a,n_z,pi_z,simoptions);
 
 %Define the functions which we wish to create time series for (from the TimeSeriesIndexes)
-TimeSeriesFn_1 = @(d_val,d_ind,aprime_val,aprime_ind,a_val,a_ind,z_val,z_ind) a_val; %Capital Stock
-TimeSeriesFn_2 = @(d_val,d_ind,aprime_val,aprime_ind,a_val,a_ind,z_val,z_ind) aprime_val-(1-delta)*a_val; %Investment
+TimeSeriesFn_1 = @(d_val,aprime_val,a_val,z_val) a_val; %Capital Stock
+TimeSeriesFn_2 = @(d_val,aprime_val,a_val,z_val) aprime_val-(1-delta)*a_val; %Investment
 TimeSeriesFn={TimeSeriesFn_1, TimeSeriesFn_2};
 
 TimeSeries=TimeSeries_Case1(TimeSeriesIndexes,Policy, TimeSeriesFn, n_d, n_a, n_z, d_grid, a_grid, z_grid,simoptions);
