798d108ab97e24eb0afb543b2f9fe93926f10a41
robertdkirkby <robertdkirkby@gmail.com>
2015-07-01 17:45:31 +1200

    Hansen (1985) replication updated

--- Hansen1985_ReturnFn.m
+++ Hansen1985_ReturnFn.m
@@ -1,9 +1,13 @@
-function F=Hansen1985_ReturnFn(d_val, aprime_val, a_val, z_val, alpha, delta, gamma)
+function F=Hansen1985_ReturnFn(d_val, aprime_val, a_val, z_val, alpha, delta, A, B, Economy)
 
 F=-Inf;
-C=exp(z_val)*(a_val^alpha)*(d_val^(1-alpha))-(aprime_val-(1-delta)*a_val);
+C=z_val*(a_val^alpha)*(d_val^(1-alpha))-(aprime_val-(1-delta)*a_val);
 if C>0
-    F=log(C)-gamma*d_val;
+    if Economy==1
+        F=log(C)+A*log(1-d_val); % Divisible labour
+    elseif Economy==2
+        F=log(C)+B*(1-d_val); % Indivisible labour
+    end
 end
 
 end
