1c7f2405f0e50de976a58365e3dfb55e6e4acaf4
Robert Kirkby <robertdkirkby@gmail.com>
2022-03-01 17:21:46 +1300

    update to v2

--- EpsteinZinPreferences.m
+++ EpsteinZinPreferences.m
@@ -27,20 +27,17 @@ ParamCombination=2; %A number from 1 to 8; determines which values of psi & gamm
 % ParamCombination=4 gives 'extreme' case of CFVRRY2012 (gamma=40, psi=0.5)
 ExtremeCase=0; %To use the 'extreme' case of CFVRRY2012 you also must set this to 1 (default=0) to change values of sigmabar and eta
 
-vfoptions.exoticpreferences=2; %Use Epstein-Zin preferences 
-% If you set this zero you would just solve the same model but with
-% standard recursive vonNeumann-Morgerstern expected utility preferences
-% (parameters gamma and psi would be irrelevant to model). This is intended
-% as purely illustrative. A serious comparison of the preference types would 
-% require you to recalibrate the model.
+vfoptions.exoticpreferences='EpsteinZin'; % Use Epstein-Zin preferences 
+% If you set this 'None' you would just solve the same model but with standard recursive vonNeumann-Morgerstern expected utility preferences
+% (parameters gamma and psi would be irrelevant to model). This is intended as purely illustrative. A serious comparison of the preference 
+% types would require you to recalibrate the model.
 
 %% Grid sizes
 n_k=200;
 n_l=31;
 n_z=25;     %25 points for productivity (z)
 n_sigma=5;  %5 points for volatility (sigma)
-sprintf('Grid sizes n_k=%i, n_l=%i, n_z=%i, n_sigma=%i',n_k,n_l,n_z,n_sigma)
-vfoptions.lowmemory=0; % If you want to make the grids larger you will need to use vfoptions.lowmemory=1
+fprintf('Grid sizes n_k=%i, n_l=%i, n_z=%i, n_sigma=%i \n',n_k,n_l,n_z,n_sigma)
 
 %% Declare parameters
 ParamCombinationSub=ind2sub_homemade([2,4],ParamCombination);
@@ -73,15 +70,13 @@ end
  
 
 %% Create the return function
-if vfoptions.exoticpreferences==2 % This if statement is only needed so that you can easily check what happens if not using Epstein-Zin preferences
+if strcmp(vfoptions.exoticpreferences,'EpsteinZin') % This if statement is only needed so that you can easily check what happens if not using Epstein-Zin preferences
     DiscountFactorParamNames={'beta','gamma','psi'}; % The 'Epstein-Zin parameters'
-elseif vfoptions.exoticpreferences==0
+else
     DiscountFactorParamNames={'beta'};
 end
 
 ReturnFn =@(l,kprime,k,z,zeta,delta,upsilon) EpsteinZinPreferences_ReturnFn(l,kprime,k,z,zeta, delta, upsilon);
-ReturnFnParamNames={'zeta','delta','upsilon'};
-
 
 %% Create grids
 % Create grids using CFVRRY2012 notation of l & k, then convert into VFI toolkit notation of d & a
@@ -96,7 +91,6 @@ k_grid=sort([linspace(3/4*k_ss,1.5*k_ss,ceil(n_k/3)),linspace(k_ss/2,2*k_ss,floo
 % Figure out the index of k_ss in k_grid (needed for drawing some graphs later on; not needed to solve model)
 [~,k_ss_index]=min(abs(k_grid-k_ss));
 
-
 %My methods also require a grid for labour supply l
 l_grid=linspace(0.1,0.65,n_l)'; % In theory you might prefer to use 0 to 1, but a quick investigation shows anything outside this range would be wasted  (investigation shows=solve with 0 to 1, look at actual policy function)
 %l_grid=linspace(0,1,n_l)';
@@ -109,14 +103,15 @@ n_a=length(a_grid); % k
 
 %% These lines (if uncommented) create the z_grid and sigma_grid following CFVRRY2012
 % This implements my understanding of Section 3.3 (mid pg 196 of CFVRR2012)
-[sigma_grid, pi_sigma]=TauchenMethod((1-Params.rho)*Params.sigmabar,Params.eta^2,Params.rho,n_sigma,1);
+Tauchen_q=1; %Parameter for the Tauchen method
+[sigma_grid,pi_sigma]=discretizeAR1_Tauchen((1-Params.rho)*Params.sigmabar,Params.rho,Params.eta,n_sigma,Tauchen_q);
 sigma_grid=exp(sigma_grid);
 
-[z_grid,pi_z]=TauchenMethod(0,sigma_grid(1)^2,Params.lambda,n_z,1);
+[z_grid,pi_z]=discretizeAR1_Tauchen(0,Params.lambda,sigma_grid(1),n_z,Tauchen_q);
 s_grid=z_grid;
 pi_s=kron(pi_sigma(1,:),pi_z);
 for sigma_c=2:n_sigma
-    [z_grid,pi_z]=TauchenMethod(0,sigma_grid(sigma_c)^2,Params.lambda,n_z,1);
+    [z_grid,pi_z]=discretizeAR1_Tauchen(0,Params.lambda,sigma_grid(sigma_c),n_z,Tauchen_q);
     % The following two should be predeclared, but am feeling lazy
     s_grid=[s_grid; z_grid];
     pi_s=[pi_s; kron(pi_sigma(sigma_c,:),pi_z)];
@@ -129,9 +124,8 @@ figure(1)
 plot(sort(s_grid))
 
 %% Now, do the value function iteration
-vfoptions.howards=20;
 vfoptions.verbose=1;
-[V, Policy]=ValueFnIter_Case1(n_d,n_a,n_s,d_grid,a_grid,s_grid, pi_s, ReturnFn, Params, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
+[V, Policy]=ValueFnIter_Case1(n_d,n_a,n_s,d_grid,a_grid,s_grid, pi_s, ReturnFn, Params, DiscountFactorParamNames, [], vfoptions);
 
 %% Generate output based on the solution
 
@@ -139,19 +133,19 @@ midpointofs=ceil(n_s/2);
 midpointsofz=ceil(n_z/2)+0:n_z:n_s-1;
 
 %% 1. Plot the Decision Rules (Figure 1/2)
-PolicyMatrix_l=l_grid(shiftdim(Policy(1,:,:),1));
-PolicyMatrix_kprime=k_grid(shiftdim(Policy(2,:,:),1));
-% Decision rule for consumption
-c_fn=@(l,kprime,k,z,zeta,delta) exp(z)*(k^zeta)*(l^(1-zeta))+(1-delta)*k-kprime;
-PolicyMatrix_c=arrayfun(c_fn, PolicyMatrix_l, PolicyMatrix_kprime, k_grid, s_grid',Params.zeta,Params.delta); % Value
+FnsToEvaluate2.c=@(l,kprime,k,z,zeta,delta) exp(z)*(k^zeta)*(l^(1-zeta))+(1-delta)*k-kprime; % consumption
+FnsToEvaluate2.l=@(l,kprime,k,z) l; % labor supply
+FnsToEvaluate2.kprime=@(l,kprime,k,z) kprime; % savings (next period assets)
+
+ValuesOnGrid=EvalFnOnAgentDist_ValuesOnGrid_Case1(Policy, FnsToEvaluate2, Params, [], n_d, n_a, n_s, d_grid, a_grid, s_grid, [], simoptions);
 
 figure(2)
-subplot(2,3,1), plot(k_grid,PolicyMatrix_c(:,midpointofs))
-subplot(2,3,2), plot(k_grid,PolicyMatrix_l(:,midpointofs))
-subplot(2,3,3), plot(k_grid,PolicyMatrix_kprime(:,midpointofs))
-subplot(2,3,4), plot(sigma_grid,PolicyMatrix_c(k_ss_index,midpointsofz))
-subplot(2,3,5), plot(sigma_grid,PolicyMatrix_l(k_ss_index,midpointsofz))
-subplot(2,3,6), plot(sigma_grid,PolicyMatrix_kprime(k_ss_index,midpointsofz))
+subplot(2,3,1), plot(k_grid,ValuesOnGrid.c(:,midpointofs))
+subplot(2,3,2), plot(k_grid,ValuesOnGrid.l(:,midpointofs))
+subplot(2,3,3), plot(k_grid,ValuesOnGrid.kprime(:,midpointofs))
+subplot(2,3,4), plot(sigma_grid,ValuesOnGrid.c(k_ss_index,midpointsofz))
+subplot(2,3,5), plot(sigma_grid,ValuesOnGrid.l(k_ss_index,midpointsofz))
+subplot(2,3,6), plot(sigma_grid,ValuesOnGrid.kprime(k_ss_index,midpointsofz))
 
 
 %% Set simulation options for toolkit and do simulations that are used for 2 & 3
@@ -162,16 +156,14 @@ simoptions.burnin=10^3;
 simoptions.simperiods=10^4;
 simoptions.iterate=0;
 
-TimeSeriesFn_c=@(l,kprime,k,z) exp(z)*(k^Params.zeta)*(l^(1-Params.zeta))+(1-Params.delta)*k-kprime;
-TimeSeriesFn_l=@(l,kprime,k,z) l;
-TimeSeriesFn_k=@(l,kprime,k,z) k;
-TimeSeriesFn_y=@(l,kprime,k,z) exp(z)*(k^Params.zeta)*(l^(1-Params.zeta));
-TimeSeriesFn_i=@(l,kprime,k,z) kprime-(1-Params.delta)*k;
-TimeSeriesFn_Rk=@(l,kprime,k,z) Params.zeta*exp(z)*(k^(Params.zeta-1))*(l^(1-Params.zeta))-Params.delta;
-TimeSeriesFn={TimeSeriesFn_c,TimeSeriesFn_l,TimeSeriesFn_k,TimeSeriesFn_y,TimeSeriesFn_i,TimeSeriesFn_Rk};
+FnsToEvaluate.c=@(l,kprime,k,z,zeta,delta) exp(z)*(k^zeta)*(l^(1-zeta))+(1-delta)*k-kprime; % consumption
+FnsToEvaluate.l=@(l,kprime,k,z) l;
+FnsToEvaluate.k=@(l,kprime,k,z) k;
+FnsToEvaluate.y=@(l,kprime,k,z,zeta) exp(z)*(k^zeta)*(l^(1-zeta));
+FnsToEvaluate.i=@(l,kprime,k,z,delta) kprime-(1-delta)*k;
+FnsToEvaluate.Rk=@(l,kprime,k,z,zeta,delta) zeta*exp(z)*(k^(zeta-1))*(l^(1-zeta))-delta;
 
-TimeSeriesIndexes=SimTimeSeriesIndexes_Case1(Policy,n_d,n_a,n_s,pi_s,simoptions);
-TimeSeries=TimeSeries_Case1(TimeSeriesIndexes, Policy, TimeSeriesFn, n_d, n_a, n_s, d_grid, a_grid, s_grid,simoptions);
+TimeSeries=TimeSeries_Case1(Policy, FnsToEvaluate, Params, n_d, n_a, n_s, d_grid, a_grid, s_grid,pi_s,simoptions);
 
 %% 2. Plot the Densities (Figure 3/4)
 % Not clear in CFVRRY2012 exactly how the density plots are created. I use
@@ -179,45 +171,57 @@ TimeSeries=TimeSeries_Case1(TimeSeriesIndexes, Policy, TimeSeriesFn, n_d, n_a, n
 
 nbins=30;
 
-[densityfn_c, edges, bins]=histcounts(TimeSeries(1,:),nbins);
+[densityfn_c, edges, bins]=histcounts(TimeSeries.c,nbins);
 xaxis_c=edges(1:end-1)+(edges(2:end)-edges(1:end-1))/2;
-[densityfn_l, edges, bins]=histcounts(TimeSeries(2,:),nbins);
+[densityfn_l, edges, bins]=histcounts(TimeSeries.l,nbins);
 xaxis_l=edges(1:end-1)+(edges(2:end)-edges(1:end-1))/2;
-[densityfn_k, edges, bins]=histcounts(TimeSeries(3,:),nbins);
+[densityfn_k, edges, bins]=histcounts(TimeSeries.k,nbins);
 xaxis_k=edges(1:end-1)+(edges(2:end)-edges(1:end-1))/2;
-[densityfn_y, edges, bins]=histcounts(TimeSeries(4,:),nbins);
+[densityfn_y, edges, bins]=histcounts(TimeSeries.y,nbins);
 xaxis_y=edges(1:end-1)+(edges(2:end)-edges(1:end-1))/2;
-% [densityfn_Rf, edges, bins]=histcounts(TimeSeries(2,:),100);
-% xaxis_Rf=edges(1:end-1)+(edges(2:end)-edges(1:end-1))/2;
-[densityfn_Rk, edges, bins]=histcounts(TimeSeries(6,:),nbins);
+% [densityfn_i, edges, bins]=histcounts(TimeSeries.i,100);
+% xaxis_i=edges(1:end-1)+(edges(2:end)-edges(1:end-1))/2;
+[densityfn_Rk, edges, bins]=histcounts(TimeSeries.Rk,nbins);
 xaxis_Rk=edges(1:end-1)+(edges(2:end)-edges(1:end-1))/2;
-
-% [densityfn_c,xaxis_c] = ksdensity(gather(TimeSeries(1,:)));
-% [densityfn_l,xaxis_l] = ksdensity(gather(TimeSeries(2,:)));
-% [densityfn_k,xaxis_k] = ksdensity(gather(TimeSeries(3,:)));
-% [densityfn_y,xaxis_y] = ksdensity(gather(TimeSeries(4,:)));
-% % [densityfn_Rf,xaxis_Rf] = ksdensity(gather(TimeSeries(1,:)));
-% [densityfn_Rk,xaxis_Rk] = ksdensity(gather(TimeSeries(6,:)));
 
 figure(3)
 subplot(3,2,1), plot(xaxis_c,densityfn_c)
+title('c')
 subplot(3,2,2), plot(xaxis_l,densityfn_l)
+title('l')
 subplot(3,2,3), plot(xaxis_k,densityfn_k)
+title('k')
 subplot(3,2,4), plot(xaxis_y,densityfn_y)
-% subplot(3,2,5), plot(xaxis_Rf,densityfn_Rf)
+title('y')
+% subplot(3,2,5), plot(xaxis_i,densityfn_i)
+% title('i')
 subplot(3,2,6), plot(xaxis_Rk,densityfn_Rk)
+title('Rk')
 
 
 %% 3. Numbers Relating to Business Cycles (Table 2/5) 
-
-BusCycStats_means=mean(TimeSeries,2);
-BusCycStats_var=100*std([log(TimeSeries(1:5,:));TimeSeries(6,:)],[],2).^2;
+FnNames=fieldnames(FnsToEvaluate);
+numFnsToEvaluate=length(FnNames);
+BusCycStats_means=zeros(numFnsToEvaluate,1);
+BusCycStats_var=zeros(numFnsToEvaluate,1);
+for ff=1:numFnsToEvaluate
+    BusCycStats_means(ff)=mean(TimeSeries.(FnNames{ff}));
+    if ff<6
+        BusCycStats_var(ff)=100*std(log(TimeSeries.(FnNames{ff}))).^2;
+    elseif ff==6
+        BusCycStats_var(ff)=100*std(TimeSeries.(FnNames{ff})).^2; % no log()
+    end        
+end
 
 Table=[BusCycStats_means(1), BusCycStats_means(4), BusCycStats_means(5), nan, 100*BusCycStats_means(6); ...
-    BusCycStats_var(1), BusCycStats_var(4), BusCycStats_var(5),nan, BusCycStats_var(6)]
+    BusCycStats_var(1), BusCycStats_var(4), BusCycStats_var(5),nan, BusCycStats_var(6)];
 
-%% Save all of the parts that relate to output
-%save ./SavedOutput/CaldaraFVillaverdeRRamirezYao2012.mat k_grid sigma_grid V Policy PolicyMatrix_c PolicyMatrix_l PolicyMatrix_kprime midpointofs midpointsofz k_ss_index TimeSeries Table
+% Write them out nicely to the command window
+for ff=1:numFnsToEvaluate
+    if ~strcmp(FnNames{ff},'i')
+        fprintf([FnNames{ff},' has a mean of %8.4f, and standard deviation of %8.4f \n'],BusCycStats_means(ff), BusCycStats_var(ff));
+    end
+end
 
 
 
