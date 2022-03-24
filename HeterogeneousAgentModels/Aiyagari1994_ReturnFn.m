369126e13b1d620aea651de9e6cab82724029dba
Robert Kirkby <robertdkirkby@gmail.com>
2022-03-24 13:46:05 +1300

    clean up comments

--- HeterogeneousAgentModels/Aiyagari1994_ReturnFn.m
+++ HeterogeneousAgentModels/Aiyagari1994_ReturnFn.m
@@ -1,19 +1,10 @@
-function F=Aiyagari1994_ReturnFn(aprime_val, a_val, s_val,alpha,delta,mu,r)
-% You can include comments in the return function file, but only on
-% seperate lines to the commands. If you try putting a comment on the same
-% line as a command (and following line is not blank) then this will cause
-% an error with the GPU codes.
-
-% A list of the parameters to be used
-% alpha
-% delta
-% mu
-% r
-
+function F=Aiyagari1994_ReturnFn(aprime, a, s,alpha,delta,mu,r)
+% The return function is essentially the combination of the utility
+% function and the constraints.
 
 F=-Inf;
 w=(1-alpha)*((r+delta)/alpha)^(alpha/(alpha-1));
-c=w*s_val+(1+r)*a_val-aprime_val; % Budget Constraint
+c=w*s+(1+r)*a-aprime; % Budget Constraint
 % c=w l_t+(1+r)a_t-a_{t+1}
 if c>0
     if mu==1
