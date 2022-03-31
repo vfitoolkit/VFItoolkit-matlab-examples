1572466bc6ab54960ef5d83121958be1ed0de2f2
Robert Kirkby <robertdkirkby@gmail.com>
2022-03-31 14:12:52 +1300

    update to v2

--- Exotic Preferences/Aiyagari1994_QuasiHyperbolic/Aiyagari1994_QuasiHyperbolic.m
+++ Exotic Preferences/Aiyagari1994_QuasiHyperbolic/Aiyagari1994_QuasiHyperbolic.m
@@ -48,7 +48,7 @@ end
 %% Set some basic variables
 
 n_k=2^10;
-n_z=251;
+n_z=21;
 n_p=0; % Normally you will want n_p=0, setting a non-zero value here activates the use of a grid on prices.
 
 %Parameters
@@ -63,16 +63,20 @@ Params.rho=0.6;
 Params.q=2; %Footnote 33 of Aiyagari(1993WP, pg 25) implicitly says that he uses q=3
 
 %% Set up the exogenous shock process
-%Create markov process for the exogenous labour productivity, l.
-[z_grid, pi_z]=TauchenMethod(0,(Params.sigma^2)*(1-Params.rho^2),Params.rho,n_z,Params.q);
+% Create markov process for the exogenous labour productivity, l.
+Tauchen_q=3; % Footnote 33 of Aiyagari(1993WP, pg 25) implicitly says that he uses q=3
+[z_grid,pi_z]=discretizeAR1_Tauchen(0,Params.rho,sqrt((1-Params.rho^2)*Params.sigma^2),n_z,Tauchen_q);
+% Note: sigma is standard deviations of s, input needs to be standard deviation of the innovations
+% Because s is AR(1), the variance of the innovations is (1-rho^2)*sigma^2
+
 [z_mean,z_variance,z_corr,~]=MarkovChainMoments(z_grid,pi_z);
 z_grid=exp(z_grid);
-% Aiyagari 1994 normalizes z_grid to make sure E[z]=1. The following lines can be uncommented to do this
-%Get some info on the markov process
+% Get some info on the markov process
 [Expectation_z,~,~,~]=MarkovChainMoments(z_grid,pi_z); %Since l is exogenous, this will be it's eqm value 
-% Normalize z_grid to make E[z]=1 hold exactly
+% Note: Aiyagari (1994) actually then normalizes l by dividing it by Expectation_z (so that the resulting process has expectation equal to 1)
 z_grid=z_grid./Expectation_z;
 [Expectation_z,~,~,~]=MarkovChainMoments(z_grid,pi_z);
+% If you look at Expectation_z you will see it is now equal to 1
 Params.Expectation_z=Expectation_z; % Need to put it in Params so it can be used as part of the general eqm eqn
 
 %% Grids
@@ -95,26 +99,19 @@ n_d=0;
 n_a=n_k;
 %n_z
 
-% Create descriptions of aggregate values as functions of d_grid, a_grid, z_grid 
-% (used to calculate the integral across the agent dist fn of whatever functions you define here)
-FnsToEvaluateParamNames(1).Names={};
-FnsToEvaluateFn_K = @(aprime_val,a_val,z_val) a_val; %We just want the aggregate assets (which is this periods state)
-FnsToEvaluate={FnsToEvaluateFn_K};
+% Create functions to be evaluated
+FnsToEvaluate.K = @(aprime,a,z) a; %We just want the aggregate assets (which is this periods state)
 
-%Now define the functions for the General Equilibrium conditions
-    %Should be written as LHS of general eqm eqn minus RHS, so that 
-    %the closer the value given by the function is to zero, the closer 
-    %the general eqm condition is to holding.
-%Note: length(AggVars) is length(FnsToEvaluate) and length(p) is length(GEPriceParamNames)
-GeneralEqmEqnParamNames(1).Names={'alpha','delta','Expectation_z'};
-GeneralEqmEqn_r = @(AggVars,p,alpha,delta,Expectation_z) p-(alpha*(AggVars(1)^(alpha-1))*(Expectation_z^(1-alpha))-delta); %The requirement that the interest rate corresponds to the marginal product of capital (net of depreciation)
-GeneralEqmEqns={GeneralEqmEqn_r};
+% Now define the functions for the General Equilibrium conditions
+    % Should be written as LHS of general eqm eqn minus RHS, so that the closer the value given by the function is to 
+    % zero, the closer the general eqm condition is to holding.
+GeneralEqmEqns.CapitalMarket = @(r,K,alpha,delta,Expectation_z) r-(alpha*(K^(alpha-1))*(Expectation_z^(1-alpha))-delta); %The requirement that the interest rate corresponds to the agg capital level
+% Inputs can be any parameter, price, or aggregate of the FnsToEvaluate
+
 
 %%
-% DiscountFactorParamNames={'beta'};
-
+% DiscountFactorParamNames was set above as part of implementing Quasi-Hyperbolic discounting.
 ReturnFn=@(aprime_val, a_val, z_val,alpha,delta,gamma,r) Aiyagari1994_ReturnFn(aprime_val, a_val, z_val,alpha,delta,gamma,r);
-ReturnFnParamNames={'alpha','delta','gamma','r'}; %It is important that these are in same order as they appear in 'Aiyagari1994_ReturnFn'
 
 %%
 
@@ -127,12 +124,11 @@ Params.r=0.04;
 
 %% Calculate the general equilibrium 
 
-vfoptions.verbose=1
 % We will just use the default options for simoptions
 simoptions=struct();
 disp('Calculating price vector corresponding to the stationary eqm')
 heteroagentoptions.verbose=1;
-[p_eqm,~,GeneralEqmCondn]=HeteroAgentStationaryEqm_Case1(n_d, n_a, n_z, n_p, pi_z, d_grid, a_grid, z_grid, ReturnFn, FnsToEvaluate, GeneralEqmEqns, Params, DiscountFactorParamNames, ReturnFnParamNames, FnsToEvaluateParamNames, GeneralEqmEqnParamNames, GEPriceParamNames,heteroagentoptions, simoptions, vfoptions);
+[p_eqm,~,GeneralEqmCondn]=HeteroAgentStationaryEqm_Case1(n_d, n_a, n_z, n_p, pi_z, d_grid, a_grid, z_grid, ReturnFn, FnsToEvaluate, GeneralEqmEqns, Params, DiscountFactorParamNames, [], [], [], GEPriceParamNames,heteroagentoptions, simoptions, vfoptions);
 
 p_eqm
 
@@ -142,7 +138,7 @@ Params.r=p_eqm.r;
 Params.w=(1-Params.alpha)*((Params.r+Params.delta)/Params.alpha)^(Params.alpha/(Params.alpha-1));
 
 disp('Calculating various equilibrium objects')
-[~,Policy]=ValueFnIter_Case1(n_d,n_a,n_z,d_grid,a_grid,z_grid, pi_z, ReturnFn, Params, DiscountFactorParamNames, ReturnFnParamNames, vfoptions);
+[~,Policy]=ValueFnIter_Case1(n_d,n_a,n_z,d_grid,a_grid,z_grid, pi_z, ReturnFn, Params, DiscountFactorParamNames, [], vfoptions);
 
 % PolicyValues=PolicyInd2Val_Case1(Policy,n_d,n_a,n_z,d_grid,a_grid);
 
@@ -152,7 +148,7 @@ StationaryDist=StationaryDist_Case1(Policy,n_d,n_a,n_z,pi_z, simoptions);
 figure(2)
 plot(cumsum(sum(StationaryDist,2)))
 
-AggVars=EvalFnOnAgentDist_AggVars_Case1(StationaryDist, Policy, FnsToEvaluate,Params, FnsToEvaluateParamNames,n_d, n_a, n_z, d_grid, a_grid,z_grid);
+AggVars=EvalFnOnAgentDist_AggVars_Case1(StationaryDist, Policy, FnsToEvaluate,Params, [],n_d, n_a, n_z, d_grid, a_grid,z_grid);
 
 % Calculate savings rate:
 % We know production is Y=K^{\alpha}L^{1-\alpha}, and that L=1
@@ -160,31 +156,27 @@ AggVars=EvalFnOnAgentDist_AggVars_Case1(StationaryDist, Policy, FnsToEvaluate,Pa
 % In equilibrium K is constant, so aggregate savings is just depreciation, which
 % equals delta*K. The agg savings rate is thus delta*K/Y.
 % So agg savings rate is given by s=delta*K/(K^{\alpha})=delta*K^{1-\alpha}
-aggsavingsrate=Params.delta*AggVars^(1-Params.alpha);
+aggsavingsrate=Params.delta*AggVars.K.Mean^(1-Params.alpha);
 
 % Calculate Lorenz curves, Gini coefficients, and Pareto tail coefficients
-FnsToEvaluateParamNames(1).Names={'w'};
-FnsToEvaluate_Earnings = @(aprime_val,a_val,z_val,w) w*z_val;
-FnsToEvaluateParamNames(2).Names={'r','w'};
-FnsToEvaluate_Income = @(aprime_val,a_val,z_val,r,w) w*z_val+(1+r)*a_val;
-FnsToEvaluateParamNames(3).Names={};
-FnsToEvaluate_Wealth = @(aprime_val,a_val,z_val) a_val;
-FnsToEvaluateFnIneq={FnsToEvaluate_Earnings, FnsToEvaluate_Income, FnsToEvaluate_Wealth};
-StationaryDist_LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluateFnIneq, Params,FnsToEvaluateParamNames, n_d, n_a, n_z, d_grid, a_grid, z_grid);
+FnsToEvaluate_Ineq.Earnings = @(aprime,a,z,w) w*z;
+FnsToEvaluate_Ineq.Income = @(aprime,a,z,r,w) w*z+(1+r)*a;
+FnsToEvaluate_Ineq.Wealth = @(aprime,a,z) a;
+LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluate_Ineq, Params,[], n_d, n_a, n_z, d_grid, a_grid, z_grid);
 
 % 3.5 The Distributions of Earnings and Wealth
 %  Gini for Earnings
-EarningsGini=Gini_from_LorenzCurve(StationaryDist_LorenzCurves(1,:));
-IncomeGini=Gini_from_LorenzCurve(StationaryDist_LorenzCurves(2,:));
-WealthGini=Gini_from_LorenzCurve(StationaryDist_LorenzCurves(3,:));
+EarningsGini=Gini_from_LorenzCurve(LorenzCurves.Earnings);
+IncomeGini=Gini_from_LorenzCurve(LorenzCurves.Income);
+WealthGini=Gini_from_LorenzCurve(LorenzCurves.Wealth);
 
 % Calculate inverted Pareto coeff, b, from the top income shares as b=1/[log(S1%/S0.1%)/log(10)] (forgammala taken from Excel download of WTID database)
 % No longer used: Calculate Pareto coeff from Gini as alpha=(1+1/G)/2; ( http://en.wikipedia.org/wiki/Pareto_distribution#Lorenz_curve_and_Gini_coefficient)
 % Recalculte Lorenz curves, now with 1000 points
-StationaryDist_LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluateFnIneq, Params,FnsToEvaluateParamNames, n_d, n_a, n_z, d_grid, a_grid, z_grid, [],1000);
-EarningsParetoCoeff=1/((log(StationaryDist_LorenzCurves(1,990))/log(StationaryDist_LorenzCurves(1,999)))/log(10)); %(1+1/EarningsGini)/2;
-IncomeParetoCoeff=1/((log(StationaryDist_LorenzCurves(2,990))/log(StationaryDist_LorenzCurves(2,999)))/log(10)); %(1+1/IncomeGini)/2;
-WealthParetoCoeff=1/((log(StationaryDist_LorenzCurves(3,990))/log(StationaryDist_LorenzCurves(3,999)))/log(10)); %(1+1/WealthGini)/2;
+LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluate_Ineq, Params,[], n_d, n_a, n_z, d_grid, a_grid, z_grid, [],1000);
+EarningsParetoCoeff=1/((log(LorenzCurves.Earnings(990))/log(LorenzCurves.Earnings(999)))/log(10));
+IncomeParetoCoeff=1/((log(LorenzCurves.Income(990))/log(LorenzCurves.Income(999)))/log(10));
+WealthParetoCoeff=1/((log(LorenzCurves.Wealth(990))/log(LorenzCurves.Wealth(999)))/log(10));
 
 
 %% Display some output about the solution
