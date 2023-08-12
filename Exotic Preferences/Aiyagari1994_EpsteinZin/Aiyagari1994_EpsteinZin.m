f298c341265894b5f491d99cb1a616376625750b
Robert Kirkby <robertdkirkby@gmail.com>
2023-08-12 16:01:54 +1200

    overhaul of EZ prefs

--- Exotic Preferences/Aiyagari1994_EpsteinZin/Aiyagari1994_EpsteinZin.m
+++ Exotic Preferences/Aiyagari1994_EpsteinZin/Aiyagari1994_EpsteinZin.m
@@ -29,34 +29,33 @@ vfoptions.exoticpreferences='EpsteinZin'; %Use Epstein-Zin preferences
 % If you turn this off you would just solve the same model but with standard recursive 
 % vonNeumann-Morgerstern expected utility preferences.
 % This is intended as purely illustrative. A serious comparison of the preference types would require you to recalibrate the model.
+% We are going to use the traditional consumption-units Epstein-Zin
+% preferences (rather than utility-units)
+vfoptions.EZutils=0;
 
 % 2. Set the appropriate preference parameters.
 % Epstein-Zin preference parameters
 if strcmp(vfoptions.exoticpreferences,'EpsteinZin')
     Params.gamma=3; % Risk aversion
     Params.psi=0.5; % Intertemporal Elasticity of substitution
+    % Need to explain which is which
+    vfoptions.EZriskaversion='gamma';
+    vfoptions.EZeis='psi';
+
 else
     Params.gamma=2; % Note that for CES parameters, gamma is the CRRA, and
                     % 1/gamma is the intertemporal elasticity of substitution
 end
 
-% 3. Minor adjustment to 'discount factors' and 'return function'.
-% Set the discount parameters. 
-if strcmp(vfoptions.exoticpreferences,'EpsteinZin')
-    DiscountFactorParamNames={'beta','gamma','psi'}; % The 'Epstein-Zin parameters' must be the last two of the discount factor parameters.
-else
-    DiscountFactorParamNames={'beta'};
-end
+% 3. Minor adjustment to 'return function'.
 % Set the return function.
 if strcmp(vfoptions.exoticpreferences,'EpsteinZin')
     % Note that gamma has been removed from return function when using Epstein-Zin preferences.
     % To do this now use Aiyagari1994_EpsteinZin_ReturnFn as return fn.
-    ReturnFn=@(aprime_val, a_val, z_val,alpha,delta,gamma,r) Aiyagari1994_EpsteinZin_ReturnFn(aprime_val, a_val, z_val,alpha,delta,gamma,r);
-    ReturnFnParamNames={'alpha','delta','gamma','r'}; %It is important that these are in same order as they appear in 'Aiyagari1994_ReturnFn'
+    ReturnFn=@(aprime, a, z,alpha,delta,gamma,r) Aiyagari1994_EpsteinZin_ReturnFn(aprime, a, z,alpha,delta,gamma,r);
     % Note that I could delete gamma from the return function entirely as it is no longer used.
 else
-    ReturnFn=@(aprime_val, a_val, z_val,alpha,delta,gamma,r) Aiyagari1994_ReturnFn(aprime_val, a_val, z_val,alpha,delta,gamma,r);
-    ReturnFnParamNames={'alpha','delta','gamma','r'}; %It is important that these are in same order as they appear in 'Aiyagari1994_ReturnFn'
+    ReturnFn=@(aprime, a, z,alpha,delta,gamma,r) Aiyagari1994_ReturnFn(aprime_val, a, z,alpha,delta,gamma,r);
 end
 % When using Epstein-Zin preferences the risk aversion is done as part of
 % the value function iteration but not as part of the return function
@@ -67,7 +66,7 @@ end
 
 %% Set some basic variables
 
-n_k=2^10;%2^9;
+n_k=2^9;%2^9;
 n_z=21; %21;
 n_p=0; % Normally you will want n_p=0, setting a non-zero value here activates the use of a grid on prices.
 
@@ -79,23 +78,17 @@ Params.gamma=3;
 Params.sigma=0.2;
 Params.rho=0.6;
 
-Params.q=3; %Footnote 33 of Aiyagari(1993WP, pg 25) implicitly says that he uses q=3
-
 %% Set up the exogenous shock process
-% Create markov process for the exogenous labour productivity, l.
-Tauchen_q=3; % Footnote 33 of Aiyagari(1993WP, pg 25) implicitly says that he uses q=3
-[z_grid,pi_z]=discretizeAR1_Tauchen(0,Params.rho,sqrt((1-Params.rho^2)*Params.sigma^2),n_z,Tauchen_q);
-% Note: sigma is standard deviations of s, input needs to be standard deviation of the innovations
-% Because s is AR(1), the variance of the innovations is (1-rho^2)*sigma^2
-
+%Create markov process for the exogenous labour productivity, l.
+[z_grid,pi_z] = discretizeAR1_FarmerToda(0,Params.rho,Params.sigma*sqrt(1-Params.rho^2),n_z);
 [z_mean,z_variance,z_corr,~]=MarkovChainMoments(z_grid,pi_z);
 z_grid=exp(z_grid);
-% Get some info on the markov process
+% Aiyagari 1994 normalizes z_grid to make sure E[z]=1. The following lines do this
+%Get some info on the markov process
 [Expectation_z,~,~,~]=MarkovChainMoments(z_grid,pi_z); %Since l is exogenous, this will be it's eqm value 
-% Note: Aiyagari (1994) actually then normalizes l by dividing it by Expectation_z (so that the resulting process has expectation equal to 1)
+% Normalize z_grid to make E[z]=1 hold exactly
 z_grid=z_grid./Expectation_z;
 [Expectation_z,~,~,~]=MarkovChainMoments(z_grid,pi_z);
-% If you look at Expectation_z you will see it is now equal to 1
 Params.Expectation_z=Expectation_z; % Need to put it in Params so it can be used as part of the general eqm eqn
 
 %% Grids
@@ -117,20 +110,21 @@ n_d=0;
 n_a=n_k;
 %n_z
 
-% Create functions to be evaluated
+% Create descriptions of aggregate values as functions of d_grid, a_grid, z_grid 
+% (used to calculate the integral across the agent dist fn of whatever functions you define here)
 FnsToEvaluate.K = @(aprime,a,z) a; %We just want the aggregate assets (which is this periods state)
 
-% Now define the functions for the General Equilibrium conditions
-    % Should be written as LHS of general eqm eqn minus RHS, so that the closer the value given by the function is to 
-    % zero, the closer the general eqm condition is to holding.
-GeneralEqmEqns.CapitalMarket = @(r,K,alpha,delta,Expectation_z) r-(alpha*(K^(alpha-1))*(Expectation_z^(1-alpha))-delta); %The requirement that the interest rate corresponds to the agg capital level
-% Inputs can be any parameter, price, or aggregate of the FnsToEvaluate
-
+%Now define the functions for the General Equilibrium conditions
+    %Should be written as LHS of general eqm eqn minus RHS, so that 
+    %the closer the value given by the function is to zero, the closer 
+    %the general eqm condition is to holding.
+%Note: length(AggVars) is length(FnsToEvaluate) and length(p) is length(GEPriceParamNames)
+GeneralEqmEqns.CapitalMarket = @(K,r,alpha,delta,Expectation_z) r-(alpha*(K^(alpha-1))*(Expectation_z^(1-alpha))-delta); %The requirement that the interest rate corresponds to the marginal product of capital (net of depreciation)
 
 %%
-% The following were already declared at the top as they are part of implementing Epstein-Zin preferences.
-% DiscountFactorParamNames
-% ReturnFn
+DiscountFactorParamNames={'beta'};
+
+% ReturnFn=@(aprime_val, a_val, z_val,alpha,delta,gamma,r) Aiyagari1994_ReturnFn(aprime_val, a_val, z_val,alpha,delta,gamma,r);
 
 %%
 
@@ -174,24 +168,24 @@ AggVars=EvalFnOnAgentDist_AggVars_Case1(StationaryDist, Policy, FnsToEvaluate,Pa
 aggsavingsrate=Params.delta*AggVars.K.Mean^(1-Params.alpha);
 
 % Calculate Lorenz curves, Gini coefficients, and Pareto tail coefficients
-FnsToEvaluate_Ineq.Earnings = @(aprime,a,z,w) w*z;
-FnsToEvaluate_Ineq.Income = @(aprime,a,z,r,w) w*z+(1+r)*a;
-FnsToEvaluate_Ineq.Wealth = @(aprime,a,z) a;
-LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluate_Ineq, Params,[], n_d, n_a, n_z, d_grid, a_grid, z_grid);
+FnsToEvaluateFnIneq.Earnings = @(aprime,a,z,w) w*z;
+FnsToEvaluateFnIneq.Income = @(aprime,a,z,r,w) w*z+(1+r)*a;
+FnsToEvaluateFnIneq.Wealth = @(aprime,a,z) a;
+StationaryDist_LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluateFnIneq, Params,[], n_d, n_a, n_z, d_grid, a_grid, z_grid);
 
 % 3.5 The Distributions of Earnings and Wealth
 %  Gini for Earnings
-EarningsGini=Gini_from_LorenzCurve(LorenzCurves.Earnings);
-IncomeGini=Gini_from_LorenzCurve(LorenzCurves.Income);
-WealthGini=Gini_from_LorenzCurve(LorenzCurves.Wealth);
+EarningsGini=Gini_from_LorenzCurve(StationaryDist_LorenzCurves.Earnings);
+IncomeGini=Gini_from_LorenzCurve(StationaryDist_LorenzCurves.Income);
+WealthGini=Gini_from_LorenzCurve(StationaryDist_LorenzCurves.Wealth);
 
 % Calculate inverted Pareto coeff, b, from the top income shares as b=1/[log(S1%/S0.1%)/log(10)] (forgammala taken from Excel download of WTID database)
 % No longer used: Calculate Pareto coeff from Gini as alpha=(1+1/G)/2; ( http://en.wikipedia.org/wiki/Pareto_distribution#Lorenz_curve_and_Gini_coefficient)
 % Recalculte Lorenz curves, now with 1000 points
-LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluate_Ineq, Params,[], n_d, n_a, n_z, d_grid, a_grid, z_grid, [],1000);
-EarningsParetoCoeff=1/((log(LorenzCurves.Earnings(990))/log(LorenzCurves.Earnings(999)))/log(10));
-IncomeParetoCoeff=1/((log(LorenzCurves.Income(990))/log(LorenzCurves.Income(999)))/log(10));
-WealthParetoCoeff=1/((log(LorenzCurves.Wealth(990))/log(LorenzCurves.Wealth(999)))/log(10));
+StationaryDist_LorenzCurves=EvalFnOnAgentDist_LorenzCurve_Case1(StationaryDist, Policy, FnsToEvaluateFnIneq, Params,[], n_d, n_a, n_z, d_grid, a_grid, z_grid, [],1000);
+EarningsParetoCoeff=1/((log(StationaryDist_LorenzCurves.Earnings(990))/log(StationaryDist_LorenzCurves.Earnings(999)))/log(10)); %(1+1/EarningsGini)/2;
+IncomeParetoCoeff=1/((log(StationaryDist_LorenzCurves.Income(990))/log(StationaryDist_LorenzCurves.Income(999)))/log(10)); %(1+1/IncomeGini)/2;
+WealthParetoCoeff=1/((log(StationaryDist_LorenzCurves.Wealth(990))/log(StationaryDist_LorenzCurves.Wealth(999)))/log(10)); %(1+1/WealthGini)/2;
 
 
 %% Display some output about the solution
