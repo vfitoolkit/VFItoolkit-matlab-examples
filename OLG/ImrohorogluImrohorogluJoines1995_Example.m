1cf9dd04b0cf00c191be13099da8d127a9eb6699
Robert Kirkby <robertdkirkby@gmail.com>
2024-08-01 12:42:29 +1200

    clean

--- OLG/ImrohorogluImrohorogluJoines1995_Example.m
+++ OLG/ImrohorogluImrohorogluJoines1995_Example.m
@@ -248,7 +248,7 @@ LifeCycleProfiles=LifeCycleProfiles_FHorz_Case1(StationaryDist,Policy,FnsToEvalu
 % social security benefits.
 
 %% To create Figures 6 and 7 you would also need the value of assets on the grid
-ValuesOnGrid=EvalFnOnAgentDist_ValuesOnGrid_FHorz_Case1(Policy, FnsToEvaluate2, Params, [], n_d, n_a, n_z, N_j, d_grid, a_grid, z_grid,[],simoptions);
+ValuesOnGrid=EvalFnOnAgentDist_ValuesOnGrid_FHorz_Case1(Policy, FnsToEvaluate2, Params, [], n_d, n_a, n_z, N_j, d_grid, a_grid, z_grid,simoptions);
 
 
 
